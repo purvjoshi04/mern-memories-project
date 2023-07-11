8d3ac1b174217681a39922609455ead23f9e0d94
Purv Joshi <101319136+purvjoshi04@users.noreply.github.com>
2023-07-11 17:34:44 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'mern-memories-project-backend.vercel.app' });
+const API = axios.create({ baseURL: 'https://mern-memories-project-backend.vercel.app/' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
